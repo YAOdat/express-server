dba7076042586f5e21c4c85d0eb66fac37fac94f
Yaser Odat <yaserodat@outlook.com>
2022-09-04 22:03:19 +0300

    added the test

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,8 +1,13 @@
 'use strict';
-
 const supertest = require('supertest');
 const server = require('../server');
 const request = supertest(server.app);
 
 
-describe()
+describe('Express Server', () => {
+    it('Home Page works', async () => {
+        const res = await request.get('/');
+        expect(res.text).toEqual('Hello World')
+    })
+
+})
