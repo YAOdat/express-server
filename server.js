8efd12e30d0c19aea2ff12244981934bfa67fd94
Yaser Odat <yaserodat@outlook.com>
2022-09-05 15:14:03 +0300

    fixed an error

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express ();
 const bodyPraser = require('body-parser');
 const people = []
+const cors = require( "cors" );
 
 app.get('/', (req, res) => {
     res.send('Hello World');
@@ -9,23 +10,26 @@ app.get('/', (req, res) => {
 
   })
 
+app.use(cors());
 app.use(bodyPraser.json());
 app.use(bodyPraser.urlencoded({extended:false}))
   
 app.post('/person', (req, res) => {
    let newPerson = {
     name: req.body.name,
-    age: req.body.age+5,
+    age: req.body.age,
     gender: req.body.gender
    }
    
     people.push(newPerson)
+    newPerson.age = JSON.parse(newPerson.age) +5;
+    console.log(newPerson)
     res.send(newPerson)
     res.status(200)
   })
 
   function start(port) {
-    app.listen(3000, () => console.log(`The server is up`));
+    app.listen(4000, () => console.log(`The server is up`));
   }
 
   module.exports = {
